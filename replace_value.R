1863744010d5327aa79120952b8844a98904e5a5
Michael <mike.oconnor@ieee.org>
2014-10-25 11:06:34 -0400

    Deleted replace_value.R - mistake that it was uploaded

--- replace_value.R
+++
@@ -1,5 +0,0 @@
-replace_value <- function(x){
-        sh <- combined_activity$activity %in% as.character(x)
-        tmp <- as.character(activities[x,1])
-        combined_activity$activity[sh] <<- tmp
-}
